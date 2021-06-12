b00e2ad3e70729793b37aca752e414e0db32df44
erntorii <erntorii@gmail.com>
2021-06-12 16:12:52 +0900

    add newTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { VFC, useState, useEffect } from 'react';
+import { FormControl, TextField } from '@material-ui/core';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 
 import { db } from './firebase';
 import './App.css';
 
 const App: VFC = () => {
   const [tasks, setTasks] = useState([{ id: '', title: '' }]);
+  const [input, setInput] = useState('');
   useEffect(() => {
     const unSub = db.collection('tasks').onSnapshot((snapshot) => {
       setTasks(
@@ -14,8 +17,26 @@ const App: VFC = () => {
     return () => unSub();
   }, []);
 
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection('tasks').add({ title: input });
+    setInput('');
+  }
+
   return (
     <div className="App">
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+
       {tasks.map((task) =>
         <h3 key={task.id}>{task.title}</h3>
       )}
